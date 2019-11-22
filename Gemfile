88e549dd1bfe79e48e2fcedfb25283ca515a614c
Drew Bomhof <dbomhof@redhat.com>
2019-11-22 14:23:25 -0500

    Add in optimist to pass in options for connection

--- Gemfile
+++ Gemfile
@@ -1,2 +1,4 @@
 source 'https://rubygems.org'
+gem 'byebug'
 gem 'catalog-api-client-ruby', '~> 1.0.0', :git => 'https://github.com/syncrou/catalog-api-client-ruby.git'
+gem 'optimist'
--- test-client.rb
+++ test-client.rb
@@ -4,21 +4,34 @@ require 'rubygems'
 require 'bundler/setup'
 
 Bundler.require
-# Load the gem
+
 require 'catalog-api-client-ruby'
 
-# Setup authorization
+require 'optimist'
+  opts = Optimist::options do
+    opt :host, "Host and base endpoint for connection", :default => "ci.cloud.redhat.com/api/catalog/v1.0"
+    opt :user_name, "Insights username", :type => :string, :default => "dbomhof@redhat.com"
+    opt :password, "Insights password", :type => :string, :default => "redhat"
+    opt :api_class, "Api to connect to", :type => :string, :default => "PortfolioApi"
+    opt :operation, "Api operation ID", :type => :string, :default => "list_portfolios"
+  end
+
+p opts
+
 CatalogApiClientRuby.configure do |config|
-  # Configure HTTP basic authorization: BasicAuth
-  config.username = 'dbomhof@redhat.com'
-  config.password = 'redhat'
+  config.host = opts[:host]
+  config.scheme = 'https'
+  config.username = opts[:user_name]
+  config.password = opts[:password]
 end
 
-api_instance = CatalogApiClientRuby::DefaultApi.new
+api_instance = Object.const_get("CatalogApiClientRuby::#{opts[:api_class]}").new
+response = nil
 
 begin
-  #Return this API document in JSON format
-  api_instance.get_documentation
+  response = api_instance.send(opts[:operation])
 rescue CatalogApiClientRuby::ApiError => e
-  puts "Exception when calling DefaultApi->get_documentation: #{e}"
+  puts "Exception when calling #{opts[:api_class]}->#{opts[:operation]}: #{e}"
 end
+
+p response
